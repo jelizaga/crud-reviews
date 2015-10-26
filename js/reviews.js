584946d162531ad6694977e0c6d5ab297c1b8c5d
Joel Elizaga <joelizaga@hotmail.com>
2015-10-25 18:18:54 -0700

    First go at parse reviews.

--- js/reviews.js
+++ js/reviews.js
@@ -1,13 +1,8 @@
-// Initializing Parse.
+// Initializing Parse and the review raty.
 window.onload = function() {
 	Parse.initialize("hnkJHK9wfuwpvFvE3rkakJRXoF1JpLyNveOV5g64", "qVGWixyvuf5EKo4RKMkkAybzbDGAEbAbiCW3fEJZ");
-	alert("Parse initialized. Creating raty.");
 	$("#ratyReview").raty();
-	alert("raty created.");
-	// Initializing a raty rating plugin.
-	// Initializing Review.
 	var Review = Parse.Object.extend("Review");
-	alert("Done.");
 	// getData for the reviews.
 	getData();
 }
@@ -18,11 +13,18 @@ $("reviewForm").submit(function() {
 	alert("Submitted.");
 
 	var newReview = new Review();
+	var stars = $("#ratyReview").raty("score");
 
+	alert("Stars: " + stars);
+
+	// Setting the newReview's content.
 	newReview.set("reviewerName", $("#reviewerName").val());
 	newReview.set("reviewTitle", $("reviewTitle").val());
-	newReview.set("reviewStars", $("#reviewStars").val());
+	newReview.set("reviewStars", $("#ratyReview").val());
 	newReview.set("reviewText", $("#reviewText").val());
+	// Setting the newReview's vote counts to zero.
+	newReview.set("upvotes", 0);
+	newReview.set("downvotes", 0);
 
 	// After setting each property, save your new instance back to your database
 	newReview.save(null, {
@@ -33,22 +35,20 @@ $("reviewForm").submit(function() {
 
 
 
-// Write a function to get data
+// Queries parse for review data.
 var getData = function() {
 	
     var query = new Parse.Query(Review);
-
 	// Set a parameter for your query -- where the website property isn't missing
     query.notEqualTo("reviewerText", "");
-
-	/* Execute the query using ".find".  When successful:
-	    - Pass the returned data into your buildList function
-	*/
+	// Executes the query above (var query); if it's successful the returned results are sent
+	// to insertReviews to be inserted onto the page.
     query.find({
         success:function(results) {
             insertReviews(results);
         }
     })
+
 }
 
 // Empties out the reviews <div>, loops through my parse data and sends each piece of data
@@ -58,15 +58,14 @@ var insertReviews = function(data) {
     for (i in data) {
         addReview(data[i]);
     }
-    /* data.forEach(functions(d) {
-        addItem(d);
-    }); */
 }
 
 var addReview = function(rev) {
-	// Get parameters (website, band, song) from the data item passed to the function
+	// Acquiring the review's parameters.
 	var reviewerName = rev.get("reviewerName");
 	var reviewTitle = rev.get("reviewTitle");
 	var reviewStars = rev.get("reviewStars");
 	var reviewText = rev.get("reviewText");
+
+	alert(reviewerName + " " + reviewTitle + " " + reviewStars + " " + reviewText);
 }
